66274fa605299f687d3a16f2b185453d553ce47a
rishabjain321 <rijain@syr.edu>
2018-10-04 15:57:16 -0400

    NPC movement
    used to move any object, person from one point to other specified points

---
+++ Patrol.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Patrol : MonoBehaviour {
+	public Transform[] patrolPoints;
+	public float moveSpeed;
+	private int currentPoint;
+
+    // Use this for initialization
+    void Start()
+    {
+        transform.position = patrolPoints[0].position;
+        currentPoint = 0;
+
+    }
+
+
+    // Update is called once per frame
+    void Update ()
+	{
+        if (transform.position != patrolPoints[currentPoint].position)
+        {
+
+            Vector3 pos = Vector3.MoveTowards(transform.position, patrolPoints[currentPoint].position, moveSpeed * Time.deltaTime);
+            GetComponent<Rigidbody>().MovePosition(pos);
+            Debug.Log("Position Reached.");
+        }
+        else currentPoint = (currentPoint + 1) % patrolPoints.Length;
+	}
+}
